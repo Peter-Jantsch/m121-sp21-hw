e1e812de3de3dc0b5447de0dac887792fb9d7100
Peter-Jantsch <75863835+Peter-Jantsch@users.noreply.github.com>
2021-02-24 16:02:25 -0600

    add hw05

---
+++ hw05/tests/q1_4.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'q1_4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 0 <= chi_wins <= 16
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> 0 <= chi_losses <= 16
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
